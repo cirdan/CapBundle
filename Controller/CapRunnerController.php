1d52d2ab902f4d1736f7dcb0b0755315d810434c
Sylvain Ferlac <sylvain@rc2c.fr>
2012-10-30 22:37:02 +0100

    on passe le gabarit global dans l'app

--- Controller/CapRunnerController.php
+++ Controller/CapRunnerController.php
@@ -16,33 +16,31 @@ class CapRunnerController extends Co
 
   public function recognizeAction(Request $request, $hash)
     {
-        // On est identifié : redirection page d'accueil
-        if($this->get("security.context")->isGranted('IS_AUTHENTICATED_FULLY') && $this->getUser()->getRunner()==$runner ){
-          return $this->redirect($this->generateUrl('sf_cap_homepage'));
+      // On est identifié : redirection page d'accueil
+      if($this->get("security.context")->isGranted('IS_AUTHENTICATED_FULLY') && $this->getUser()->getRunner()==$runner ){
+        return $this->redirect($this->generateUrl('sf_cap_homepage'));
+      }
+      // Dans tous les cas on supprime le runner en session
+      $session=$this->get("session");
+      $session->remove('runnerId');
+      // On trouve le runner par son hash
+      $em = $this->getDoctrine()->getEntityManager();
+      $runner = $em->getRepository('SFCapBundle:CapRunner')->findOneByHash($hash);
+      // A-t-il un user associé ?
+      if($runner){
+        $user=$em->getRepository('SFCapUserBundle:CapUser')->findOneBy(array('runner' => $runner->getId()));
+        if($user){
+          return $this->redirect($this->generateUrl('login'));
         }
-        $em = $this->getDoctrine()->getEntityManager();
-        $runner=$em->getRepository('SFCapBundle:CapRunner')->findOneByHash($hash);
-        if($runner){
-          $user = $em->createQuery("select u from SFCapUserBundle:CapUser u where u.runner=?1")
-                    ->setParameter(1, $runner->getId()) // or 
-                    ->getResult();
-          if($user){
-            $user=array_pop($results);
-          }
-          //$user=$em->getRepository('SFCapUserBundle:CapUser')->findOneBy(array('runner' => $runner->getId()));
-          if($user){
-            return $this->redirect($this->generateUrl('login'));
-          }
-        }
-        $session=$this->get("session");
-        $session->invalidate();
-        $this->get("security.context")->setToken(null);
-        if($runner){
-          $session->set('runnerId',$runner->getId());
-        }else{
-          $session->remove('runnerId');
-        }
-    return $this->redirect($this->generateUrl('sf_cap_homepage'));
+      }
+      // On déconnecte l'utilisateur courant
+      $this->get("security.context")->setToken(null);
+      // On met le runner en session
+      if($runner){
+        $session->set('runnerId',$runner->getId());
+      }
+      // Et direction la homepage...
+      return $this->redirect($this->generateUrl('sf_cap_homepage'));
     }
 
   public function byeAction(Request $request)
